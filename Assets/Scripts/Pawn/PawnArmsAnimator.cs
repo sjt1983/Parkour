16ce1ff9e58c50380f25f78f21f79c48c55a6858
sjt1983 <sjtomich@gmail.com>
2023-03-03 10:18:58 -0600

    Really good weapon equipping animations

--- Assets/Scripts/Pawn/PawnArmsAnimator.cs
+++ Assets/Scripts/Pawn/PawnArmsAnimator.cs
@@ -1,55 +1,120 @@
-
 using UnityEngine;
 
+//Used to animate the pawns arms.
 public class PawnArmsAnimator : MonoBehaviour
 {
+    //Reference to the pawn.
     [SerializeField]
     private Pawn pawn;
     
+    //Reverence to the animator.
     [SerializeField]
     private Animator pawnArmsAnimator;
 
+    /*** Item switch animations/pawn properties need to happen on a timer and in-sync ***/
+
+    //Which item we will equip
+    private EquippableItem itemToEquip;
+    //Which state the equipping script is in.
+    private EquipState equipState = EquipState.WAITING;
+
+    /*** I do a neat trick where, instead of creating animations for each weapon to "Switch" I just move the arms backwards diagonally down ***/
+    //default position of the pawns arms.
+    private Vector3 defaultPawnArmsPosition;
+    //target position of the pawns arms,
+    private Vector3 targetPawnArmsPosition;
+    //How fast we should lerp the arms when switching weapons. This should be half the total time you want to switch weapons.
+    private const float WEAPON_SWITCH_LERP_TIME = .25f;
+    //Tracker for how long we hagve lerped for.
+    private float lerpTimer = 0f;
+
+    //The default animation controller, we override this for each weapon.
     RuntimeAnimatorController defaultRuntimeAnimationController;
 
     private void Start()
     {
+        //Initialize the class.
         defaultRuntimeAnimationController = pawnArmsAnimator.runtimeAnimatorController;
+        //Set the default arms position
+        defaultPawnArmsPosition = transform.localPosition;
+        //Set the target arms position
+        targetPawnArmsPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - .5f, transform.localPosition.z - .5f);
     }
 
     private void Update()
     {
+        //Determine if we should be playing the run animation or not.
         if (pawn.IsGrounded && pawn.IsMovingFasterThan(2f) && !pawn.IsSliding)
-        {
             pawnArmsAnimator.SetBool("Running", true);
-        }
         else
-        {
             pawnArmsAnimator.SetBool("Running", false);
-        }
+
+        //See if we need to equip the item.
+        DoEquipItem();
+
+        UIManager.Instance.DebugText3 = equipState.ToString();
     }
     
+    //Trigger an animation.
     public void SetTrigger(string trigger)
     {
-        pawnArmsAnimator.SetTrigger("Vault");
+        pawnArmsAnimator.SetTrigger(trigger);
     }
 
+    //Reset an animation trigger.
     public void ResetTrigger(string trigger)
     {
-        pawnArmsAnimator.ResetTrigger("Vault");
+        pawnArmsAnimator.ResetTrigger(trigger);
     }
 
-    public void Play(string animation)
+    //Triggers the animation for equipping a new item.
+    public void HandleEquipItem(EquippableItem equippableItem)
     {
-        pawnArmsAnimator.Play(animation);
+        itemToEquip = equippableItem;
+        equipState = EquipState.LOWER;
     }
 
-    public void OverrideAnimations(AnimatorOverrideController animatorOverrideController)
+    //Do the actual equipping animation.
+    private void DoEquipItem()
     {
-        pawnArmsAnimator.runtimeAnimatorController = animatorOverrideController;
+        //Waiting just means we are waiting for someome to trigger the equip animation
+        if (equipState == EquipState.WAITING)
+        {
+            lerpTimer = 0f;
+        }
+        //Lowering means we are moving the arms out of the way.
+        else if (equipState == EquipState.LOWER) {
+            lerpTimer += Time.deltaTime;
+            //Lerp the arms out the way!
+            transform.localPosition = Vector3.Lerp(defaultPawnArmsPosition, targetPawnArmsPosition, lerpTimer / WEAPON_SWITCH_LERP_TIME);
+            //Basically, if we are "close enough" just set the position manually and move on!
+                        
+            if (lerpTimer >= WEAPON_SWITCH_LERP_TIME)
+            {
+                lerpTimer = 0f;
+                pawnArmsAnimator.runtimeAnimatorController = itemToEquip != null ? itemToEquip.AnimatorOverrideController : defaultRuntimeAnimationController;
+                equipState = EquipState.RAISE;
+            }
+        }
+        //Raising is bring our arms back up.
+        else if (equipState == EquipState.RAISE)
+        {
+            lerpTimer += Time.deltaTime;
+            //Lerp the arms back!
+            transform.localPosition = Vector3.Lerp(targetPawnArmsPosition, defaultPawnArmsPosition, lerpTimer / WEAPON_SWITCH_LERP_TIME);
+            //Basically, if we are "close enough" just set the position manually and move on!
+            if (lerpTimer >= WEAPON_SWITCH_LERP_TIME)
+            {
+                transform.localPosition = defaultPawnArmsPosition;
+                equipState = EquipState.WAITING;
+            }
+        }
     }
+}
 
-    public void ClearOverrides()
-    {
-        pawnArmsAnimator.runtimeAnimatorController = defaultRuntimeAnimationController;
-    }
+enum EquipState
+{
+    WAITING,
+    LOWER,
+    RAISE
 }
--- Assets/Scripts/Pawn/PawnInventory.cs
+++ Assets/Scripts/Pawn/PawnInventory.cs
@@ -1,5 +1,5 @@
+using System.Collections;
 using UnityEngine;
-
 
 //Class representing the pawns inventory.
 public class PawnInventory : MonoBehaviour
@@ -25,34 +25,40 @@ public class PawnInventory : MonoBehaviour
     //Current active inventory slow.
     public int activeSlot = -1;
 
+    //Flag to indicate a current "item switch" is happening.
+    private bool equippingItem = false;
+
+    private const float ITEM_SWITCH_DELAY = .25f;
+        
     private void Update()
     {
-        if (pawnInput.EquipSlot1PressedThisFrame)
-            EquipItem(0);
-        else if (pawnInput.EquipSlot2PressedThisFrame)
-            EquipItem(1);
-        else if (pawnInput.EquipSlot3PressedThisFrame)
-            EquipItem(2);
-        else if (pawnInput.EquipSlot4PressedThisFrame)
-            EquipItem(3);
-        else if (pawnInput.UnequipPressedThisFrame)
-            UnequipItem();
+        //For now, we only need to handle the equipment buttons being pressed.
+        if (!equippingItem)
+        {
+            if (pawnInput.EquipSlot1PressedThisFrame)
+                EquipItem(0);
+            else if (pawnInput.EquipSlot2PressedThisFrame)
+                EquipItem(1);
+            else if (pawnInput.EquipSlot3PressedThisFrame)
+                EquipItem(2);
+            else if (pawnInput.EquipSlot4PressedThisFrame)
+                EquipItem(3);
+            else if (pawnInput.UnequipPressedThisFrame)
+                UnequipItem();
+        }
 
-        //this is our inventory UI.....for now!
-        UIManager.Instance.DebugText3 = ItemSlots[0] != null ? ItemSlots[0].ItemName : "";
     }
 
     //Pickup an item.
     public void PickupItem(EquippableItem equippableItem)
     {
-        //Set the transform.
-        equippableItem.transform.parent = inventoryGameObject;
-
         //Find an empty slot for the item.
         for (int x = 0; x < 4; x++)
         {
             if (ItemSlots[x] == null)
             {
+                //Set the transform.
+                equippableItem.transform.parent = inventoryGameObject;
                 ItemSlots[x] = equippableItem;
                 return;
             }
@@ -68,43 +74,57 @@ public class PawnInventory : MonoBehaviour
         if (activeSlot == slot)
             return; 
 
+        //Equip the item in the slot, if there is one.
+        if (ItemSlots[slot] != null)
+        {
+            equippingItem = true;
+            pawnArmsAnimator.HandleEquipItem(ItemSlots[slot]);
+
+            //We need to take into account animations before doing the switch because switching triggers showing/hiding of meshes.
+            StartCoroutine(DoEquipItem(slot, ITEM_SWITCH_DELAY));
+        }            
+    }
+
+    //Coroutine for doing the item switch
+    private IEnumerator DoEquipItem(int slot, float time)
+    {
+        yield return new WaitForSeconds(time);
+
         //If we do have an active slot, AND it has an item, unequip the item.
         if (activeSlot != -1 && ItemSlots[activeSlot] != null)
         {
             ItemSlots[activeSlot].UnequipItem();
-            pawnArmsAnimator.ClearOverrides();
-        }            
+        }
+        ItemSlots[slot].transform.parent = itemBone;
+        ItemSlots[slot].EquipItem();
 
-        //Set the active slot.
         activeSlot = slot;
-
-        //Equip the item in the slot, if there is one.
-        if (ItemSlots[slot] != null)
-        {
-            ItemSlots[slot].transform.parent = itemBone;
-            ItemSlots[slot].EquipItem();
-            pawnArmsAnimator.OverrideAnimations(ItemSlots[slot].AnimatorOverrideController);
-        }            
+        equippingItem = false;
     }
 
     //Unequip any active Item
     public void UnequipItem()
     {
-        //aka if there is an active slot with an item.
+        equippingItem = true;
+        pawnArmsAnimator.HandleEquipItem(null);
+
+        //We need to take into account animations before doing the switch because switching triggers showing/hiding of meshes.
+        StartCoroutine(DoUnequipItem(ITEM_SWITCH_DELAY));
+    }
+
+    //Coroutine for unequipping an item.
+    private IEnumerator DoUnequipItem(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        //If there is an active slot with an item.
         if (activeSlot > -1 && ItemSlots[activeSlot] != null)
         {
             ItemSlots[activeSlot].UnequipItem();
-            pawnArmsAnimator.ClearOverrides();
             ItemSlots[activeSlot].transform.parent = inventoryGameObject;
-        }            
-
-        //Set the active slot to -1;
-        activeSlot = -1;
-    }
-
-    public EquippableItem GetEquippableItem()
-    {
-        return activeSlot == -1 ? null : ItemSlots[activeSlot];
+            activeSlot = -1;
+            equippingItem = false;
+        }
     }
 
     //Drop an item.
